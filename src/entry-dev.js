31cfbd050e5413820f06926222c10ccf1f002d3a
Martin Marosi <marvusm.mmi@gmail.com>
2020-11-05 15:28:47 +0100

    Update scalprum to use route dismount.

--- src/entry-dev.js
+++ src/entry-dev.js
@@ -8,31 +8,28 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { init } from './Store';
 import logger from 'redux-logger';
 import messages from '../locales/data.json';
-import { render, unmountComponentAtNode } from 'react-dom';
 import { initializeApp } from '@scalprum/core';
 
-// const url = new URL(document.currentScript.src);
-// eslint-disable-next-line no-undef
-// __webpack_public_path__ = `${url.origin}/${__webpack_public_path__}`;
-
 console.log('ADVISOR DEV');
+
+const Advisor = () => (
+    <IntlProvider locale={navigator.language.slice(0, 2)} messages={messages} onError={console.log}>
+        <Provider store={init(logger).getStore()}>
+            <Router basename="/insights/advisor/foo">
+                <React.Fragment>
+                    <NotificationsPortal />
+                    <App />
+                </React.Fragment>
+            </Router>
+        </Provider>
+    </IntlProvider>
+);
 
 initializeApp({
     id: 'advisor',
     name: 'advisor',
-    unmount: () => unmountComponentAtNode(document.getElementById('advisor-root')),
+    unmount: () => console.log('unmounting advisor'),
     update: console.log,
-    mount: () => render(
-        <IntlProvider locale={navigator.language.slice(0, 2)} messages={messages} onError={console.log}>
-            <Provider store={init(logger).getStore()}>
-                <Router basename="/insights/advisor/foo">
-                    <React.Fragment>
-                        <NotificationsPortal />
-                        <App />
-                    </React.Fragment>
-                </Router>
-            </Provider>
-        </IntlProvider>,
-        document.getElementById('advisor-root')
-    )
+    // eslint-disable-next-line react/display-name
+    mount: () => <Advisor />
 });
